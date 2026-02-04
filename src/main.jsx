897387b605111629ee007805ddf5e97b4912cb13
Adityaawaik <adityaawaik04@gmail.com>
2026-02-04 20:23:19 +0530

    files updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateAccountPage from "./Components/CreateAccountPage.jsx";
 import RegisteredPage from "./Components/RegisteredPage.jsx";
-import PopXLogic from "./Store/PopXLogic.jsx";
 import AccountSetting from "./Components/AccountSetting.jsx";
+import PopXLogic from "./Store/PopXLogic.jsx";
 
-const router = createBrowserRouter(
-  [
-    { path: "/", element: <App /> },
-    { path: "/createAccount", element: <CreateAccountPage /> },
-    { path: "/registeredAccount", element: <RegisteredPage /> },
-    { path: "/accountCreated", element: <AccountSetting /> },
-  ],
-  {
-    basename: "/PopX-Account",
-  }
-);
+const router = createHashRouter([
+  { path: "/", element: <App /> },
+  { path: "/createAccount", element: <CreateAccountPage /> },
+  { path: "/registeredAccount", element: <RegisteredPage /> },
+  { path: "/accountCreated", element: <AccountSetting /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
